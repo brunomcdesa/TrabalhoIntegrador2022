f36d8baeef7ea7bf6ed3fd7e061dee480325f7d2
jessica.gloor <jessicagloor@hotmail.com>
2022-10-14 20:36:35 -0300

    Criação do Projeto.

---
+++ ProjetoPetShop/ProjetoPetShop/Data/PetContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoPetShop.Data
+{
+    public class PetContext : DbContext
+    {
+        public PetContext(DbContextOptions<PetContext> opt) : base(opt)
+        {
+
+        }
+    }
+}
