3f23dc9d41ffe59dc53bb05c5e40dfa43d12f43d
thitoGloor <thito_gloor775@hotmail.com>
2022-10-20 20:12:21 -0300

    criacao da model servico

---
+++ ProjetoPetShop/ProjetoPetShop/Model/Servico.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoPetShop.Model
+{
+    public class Servico
+    {
+        [Key]
+        public int idServico { get; set; }
+        [Required]
+        public string tipoServico { get; set; }
+        public double valor { get; set; }
+
+        public Servico (int idServico, string tipoServico, double valor)
+        {
+            this.idServico = idServico;
+            this.tipoServico = tipoServico;
+            this.valor = valor;
+        }
+        public Servico()
+        {
+
+        }
+    }
+}
