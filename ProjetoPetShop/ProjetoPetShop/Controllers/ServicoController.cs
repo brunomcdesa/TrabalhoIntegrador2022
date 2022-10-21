91ee84b61efe61f1c4ad6430365cc3eeb975db02
thitoGloor <thito_gloor775@hotmail.com>
2022-10-20 21:29:29 -0300

    criacao da controller ate o get

--- ProjetoPetShop/ProjetoPetShop/Controllers/ServicoController.cs
+++ ProjetoPetShop/ProjetoPetShop/Controllers/ServicoController.cs
@@ -1,6 +1,35 @@
-﻿namespace ProjetoPetShop.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoPetShop.Data;
+using ProjetoPetShop.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoPetShop.Controllers
 {
-    public class ServicoController
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class ServicoController : ControllerBase
     {
+     private static  List<Servico> servicos = new List<Servico>();
+     public readonly PetContext _context;
+
+        public ServicoController(PetContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public void AddServico([FromBody] Servico servico)
+        {
+            _context.Servicos.Add(servico);
+            _context.SaveChanges();
+        }
+        [HttpGet]
+        public IActionResult buscarServicos()
+        {
+            return Ok(_context.Servicos.ToList());
+        }
+
     }
+
 }
--- ProjetoPetShop/ProjetoPetShop/Data/PetContext.cs
+++ ProjetoPetShop/ProjetoPetShop/Data/PetContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjetoPetShop.Model;
 
 namespace ProjetoPetShop.Data
 {
     public class PetContext : DbContext
     {
+        public DbSet<Servico> Servicos { get; set; }
         public PetContext(DbContextOptions<PetContext> opt) : base(opt)
         {
 
