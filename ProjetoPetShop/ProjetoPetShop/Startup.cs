3658b795c93f7ad02fdfea601c29092b2c4b6446
jessica.gloor <jessicagloor@hotmail.com>
2022-11-17 21:30:05 -0300

    arrumando o codigo para consumir a api

--- ProjetoPetShop/ProjetoPetShop/Startup.cs
+++ ProjetoPetShop/ProjetoPetShop/Startup.cs
@@ -46,7 +46,7 @@ namespace ProjetoPetShop
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjetoPetShop v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
